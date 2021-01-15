5382915df3e822b6dac2954d564b1b7a38a14c1b
TimoxaV <vadim2493@gmail.com>
2021-01-15 14:41:07 +0200

    added mapping in first controller

--- src/main/java/com/webchat/bootchat/controllers/FirstController.java
+++ src/main/java/com/webchat/bootchat/controllers/FirstController.java
@@ -1,9 +1,11 @@
 package com.webchat.bootchat.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+    @GetMapping("/first")
     public String sayFirst() {
         return "Hello from first controller";
     }
